a261b3b4b1ca66e2be891e19a5aa0375df52f5ac
IntellWAR <korneev.pavel@phystech.edu>
2022-02-21 17:15:52 +0300

    Broadcast2

--- seminars/2sem/seminar3/broadcastClient.c
+++ seminars/2sem/seminar3/broadcastClient.c
@@ -11,27 +11,30 @@ const int MAX_SIZE = 4096;
 int main(int argc, char *argv[]) {
   int socket_fd, a = 1;
   char buffer[MAX_SIZE];
-  struct sockaddr_in client_addr;
-  socklen_t len = sizeof(client_addr);
+  
   socket_fd = socket(AF_INET, SOCK_DGRAM, 0);
-  struct sockaddr_in socket_info;
-  socket_info.sin_family = AF_INET;
-  socket_info.sin_port = htons(27312);
-  socket_info.sin_addr.s_addr = htonl(INADDR_BROADCAST);
+  
+  struct sockaddr_in server_addr;
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_port = htons(27312);
+  server_addr.sin_addr.s_addr = htonl(INADDR_BROADCAST);
+
+  socklen_t len = sizeof(server_addr);
 
   setsockopt(socket_fd, SOL_SOCKET, SO_BROADCAST, &a, sizeof(a));
-  bind(socket_fd, (struct sockaddr *)&socket_info, sizeof(socket_info));
+  bind(socket_fd, (struct sockaddr *)&server_addr, sizeof(server_addr));
   sendto(socket_fd, argv[1], strlen(argv[1]), MSG_CONFIRM,
-         (struct sockaddr *)&socket_info, sizeof(socket_info));
+         (struct sockaddr *)&server_addr, sizeof(server_addr));
 
   int n = recvfrom(socket_fd, buffer, MAX_SIZE, MSG_WAITALL,
-                   (struct sockaddr *)&client_addr, &len);
+                   (struct sockaddr *)&server_addr, &len);
+  printf ("%d", n);
   buffer[n] = '\n';
   buffer[n + 1] = '\0';
   write(1, buffer, n + 1);
-  printf("Ansv from: %d\n", ntohs(client_addr.sin_port));
+  printf("Ansv from: %d\n", ntohs(server_addr.sin_port));
   sendto(socket_fd, buffer, strlen(buffer), MSG_CONFIRM,
-         (struct sockaddr *)&client_addr, sizeof(client_addr));
+         (struct sockaddr *)&server_addr, sizeof(server_addr));
 
   return 0;
 }
--- seminars/2sem/seminar3/broadcastServ.c
+++ seminars/2sem/seminar3/broadcastServ.c
@@ -16,17 +16,18 @@ int main() {
 
   socket_fd = socket(AF_INET, SOCK_DGRAM, 0);
 
-  struct sockaddr_in socket_info;
-  socket_info.sin_family = AF_INET;
-  socket_info.sin_port = htons(27312);
-  socket_info.sin_addr.s_addr = htonl(INADDR_ANY);
+  struct sockaddr_in server_addr;
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_port = htons(27312);
+  server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
 
   setsockopt(socket_fd, SOL_SOCKET, SO_BROADCAST, &a, sizeof(a));
-  bind(socket_fd, (struct sockaddr *)&socket_info, sizeof(socket_info));
+  bind(socket_fd, (struct sockaddr *)&server_addr, sizeof(server_addr));
 
   while (1) {
     int n = recvfrom(socket_fd, buffer, MAX_SIZE, MSG_WAITALL,
                      (struct sockaddr *)&client_addr, &len);
+    printf ("%d", n);
     buffer[n] = '\n';
     buffer[n + 1] = '\0';
     write(1, buffer, n + 1);
